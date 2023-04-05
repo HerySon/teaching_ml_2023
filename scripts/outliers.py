d5eead588cea25a429f13788fc2e83dc657f95b4
Joelleuh <joelle.sabourdy@ynov.com>
2023-04-05 14:57:28 +0200

    add optional sklearn method

--- scripts/outliers.py
+++ scripts/outliers.py
@@ -1,31 +1,42 @@
 import numpy as np
 import pandas as pd
 
-def outliers_process(df, columns, method = 'nan', k=1.5):
+def outliers_process(df, columns, method = 'nan', k=1.5, sklearn_method=None):
     """
     Detects and handles outliers in a pandas dataframe using Interquartile Range.
     Args:
         df : pandas dataframe (The input dataframe)
-        columns : list of str (The list of column names to handle outliers for))
-        method : str, optional (default='nan').
+        columns : list of str (The list of column names to handle outliers for)
+        method : str (default='nan').
             The method to use for handling outliers. Available methods are 'nan', mean', 'median', and 'drop'
-        k : float (The multiplier for the IQR range. Default is 1.5)
+        k : float (Default=1.5)
+            The multiplier for the IQR range
+        sklearn_method: str, optional (default=None)
+            'IsolationForest' method from Scikit-learn is use for detecting outliers
     Returns:
         df : the dataframe with outliers handled
     """
     df_outliers = df.copy()
+
+    if sklearn_method == 'IsolationForest':
+        # Fit the IsolationForest estimator
+        isof = IsolationForest()
+        isof.fit(df_outliers[columns])
+        outlier_pred = isof.predict(df_outliers[columns])
+        outliers_condition = outlier_pred == -1
+        
+    else:
+        # InterQuartile Range method
+        Q1 = df.quantile(0.25)
+        Q3 = df.quantile(0.75)
+        IQR = Q3 - Q1
     
-    # InterQuartile Range method
-    Q1 = df.quantile(0.25)
-    Q3 = df.quantile(0.75)
-    IQR = Q3 - Q1
+        # Calculate lower and upper bounds for outliers detection
+        min = Q1 - k * IQR
+        max = Q3 + k * IQR
     
-    # Calculate lower and upper bounds for outliers detection
-    min = Q1 - k * IQR
-    max = Q3 + k * IQR
-    
-    # conditions
-    outliers_condition = ((df_outliers[columns] < min) | (df_outliers[columns] > max))
+        # conditions
+        outliers_condition = ((df_outliers[columns] < min) | (df_outliers[columns] > max))
 
     # handle outliers
         # replace outliers with NaN value
@@ -37,11 +48,11 @@ def outliers_process(df, columns, method = 'nan', k=1.5):
         # replace outliers with median value
     elif method == 'median':
         median = df_outliers[columns].median()
-        df_outliers = df_outliers[columns].mask(outliers_condition, median, axis=1)
+        df_outliers = df_outliers.mask(outliers_condition, median, axis=1)
         # replace outliers with mean value
     elif method == 'mean':
         mean = df_outliers[columns].mean()
-        df_outliers = df_outliers[columns].mask(outliers_condition, mean, axis=1)
+        df_outliers = df_outliers.mask(outliers_condition, mean, axis=1)
     else:
         return df
     return df_outliers
