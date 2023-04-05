d2091fdacf47c07b7281b2b33845b239033ab92a
guillaumechupe <74655233+guillaumechupe@users.noreply.github.com>
2023-04-05 17:22:51 +0200

    V1 Tache4.0 Visualisation par Guillaume CHUPE

---
+++ 4.0_Visualisation_GuillaumeCHUPE/#4.0_visualisation_GC.py
@@ -0,0 +1,66 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud
+
+
+def plot_wordcloud(data, column):
+    """
+    Displays a wordcloud for the given column.
+
+    Args:
+        data (DataFrame): The dataframe to use.
+        column (str): The name of the column to display.
+    """
+    text = ' '.join(data[column].astype(str).tolist())
+    wordcloud = WordCloud(width=800, height=800, background_color='white', colormap='Set2', min_font_size=10).generate(text)
+
+    plt.figure(figsize=(8, 8), facecolor=None)
+    plt.imshow(wordcloud)
+    plt.axis('off')
+    plt.tight_layout(pad=0)
+    plt.show()
+
+
+def plot_density(data, column):
+    """
+    Displays a density plot for the given column.
+
+    Args:
+        data (DataFrame): The dataframe to use.
+        column (str): The name of the column to display.
+    """
+    data[column].plot(kind='density')
+    plt.show()
+
+
+def plot_unique_values(data):
+    """
+    Displays a barplot for each column of the given dataframe,
+    showing the number of unique values for each column.
+
+    Args:
+        data (DataFrame): The dataframe to use.
+    """
+    unique_vals = []
+    for col in data.columns:
+        unique_vals.append(data[col].nunique())
+
+    plt.bar(data.columns, unique_vals)
+    plt.xticks(rotation=90)
+    plt.xlabel('Columns')
+    plt.ylabel('Number of unique values')
+    plt.show()
+
+
+def plot_histogram(data, column):
+    """
+    Displays a histogram for the given column.
+
+    Args:
+        data (DataFrame): The dataframe to use.
+        column (str): The name of the column to display.
+    """
+    data[column].hist()
+    plt.xlabel(column)
+    plt.ylabel('Frequency')
+    plt.show()
