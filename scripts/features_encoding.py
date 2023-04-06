9f28598f9962cb477517495358d9015c478f1db7
Joelleuh <joelle.sabourdy@ynov.com>
2023-04-06 09:28:44 +0200

    modification de l'encoder -> OrdinalEncoder

--- scripts/features_encoding.py
+++ scripts/features_encoding.py
@@ -1,25 +1,23 @@
 import pandas as pd
-from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import OrdinalEncoder 
 
 
-def non_numeric_features_encoder(df, columns, sparse=True):
+def non_numeric_features_encoder(df, columns):
     """
-    Encode non-numeric features in a pandas dataframe using OneHotEncoder from Scikit-Learn.
+    Encode non-numeric features in a pandas dataframe using OrdinalEncoder from Scikit-Learn.
+    The input to this transformer should be an array-like of integers or strings, denoting the values taken on by categorical (discrete) features.
+    The features are converted to ordinal integers. This results in a single column of integers (0 to n_categories - 1) per feature.
     Args:
         df : pandas dataframe (The input dataframe)
         columns : list of str (The list of column names to encode)
-        sparse : bool (Default=True)
-            Will return sparse matrix if set True else will return an array.
     Returns:
         df : the encoded dataframe
     """
-    # create a OneHotEncoder object
-    encoder = OneHotEncoder(sparse_output=sparse)
+    # create a OrdinalEncoder object
+    encoder = OrdinalEncoder()
     # encode features selected
     encoded_features = encoder.fit_transform(df[columns])
     # create a list of the new features names
     df[encoder.get_feature_names_out()] = encoded_features
-    # drop original features
-    df.drop(columns, axis=1, inplace=True)
     # return new dataframe with features encoded
     return df
