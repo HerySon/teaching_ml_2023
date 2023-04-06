428ba1798cf2757f5cdaef82a7c52ac91b87b24a
Yonuko <sacha.eghiazarian@ynov.com>
2023-04-06 16:31:42 +0200

    Ajout de la fonction filter et de son exemple

--- scripts/datas_filter.py
+++ scripts/datas_filter.py
@@ -4,17 +4,56 @@ class datas_filter():
     def __init__(self) -> None:
         pass
 
-    def filter(self, datas: pd.DataFrame, type = "numerical") -> pd.DataFrame:
+    def filter(self, datas: pd.DataFrame, type : str = "numerical", category_count : int = 100) -> pd.DataFrame:
         """Permet de filtrer les données en entrée
             datas : pd.DataFrame
                 Données en entrée
             type : str
                 Type de données à filtrer
-                Valeurs possibles : "numerical", "ordonal", "non-ordinales"
+                Valeurs possibles : 
+                    "numerical" : valeurs numériques, 
+                    "ordonal" : catégories ordinales, 
+                    "non-ordinales" : catégories non-ordinales,
+                    "categorical" : catégories ordinales et non-ordinales, 
+            category_count : int
+                Nombre de catégories maximum pour une feature 
+                (Ne fonctionne que si type != "numerical")
             return : pd.DataFrame
                 Données filtrées
         """
-        pass
+        # On vérifie que les données en entrée soient du bon type
+        assert isinstance(datas, pd.DataFrame), "datas doit être un pd.DataFrame"
+        assert isinstance(type, str), "type doit être un str"
+        assert isinstance(category_count, int), "category_count doit être un int"
+
+        # On vérifie que le type soit valide
+        assert type in ["numerical", "ordinal", "non-ordinal", "categorical"], "type doit être dans ['numerical', 'ordinal', 'non-ordinal', 'categorical']"
+
+        # On vérifie que le nombre de catégories soit valide
+        assert category_count > 0, "category_count doit être supérieur à 0"
+
+        if type == "numerical":
+            # On récupère les features numériques
+            features = datas.select_dtypes(include=["integer", "float"]).columns
+        elif type == "ordinal":
+            # On récupère les features catégorielles ordinales
+            features = datas.select_dtypes(include=["category"]).columns
+            for feature in features:
+                # On vérifie que la feature soit ordonnée et qu'elle ne contienne pas trop de catégories
+                if not datas[feature].cat.ordered or datas[feature].nunique() > category_count:
+                    features = features.drop(feature)
+        elif type == "non-ordinal":
+            # On récupère les features catégorielles non-ordinales
+            features = datas.select_dtypes(include=["category"]).columns
+            for feature in features:
+                # On vérifie que la feature ne soit pas ordonnée et qu'elle ne contienne pas trop de catégories
+                if datas[feature].cat.ordered or datas[feature].nunique() > category_count:
+                    features = features.drop(feature)
+        elif type == "categorical":
+            # On récupère les features catégorielles
+            features = datas.select_dtypes(include=["category"]).columns
+        return datas[features]
+
 
     def downcast(self, datas: pd.DataFrame, features = None) -> pd.DataFrame:
         """Permet de réduire la taille mémoire des données
@@ -32,7 +71,7 @@ class datas_filter():
         # Si aucune feature n'est spécifiée, on prend toutes les features
         if features is None:
             features = datas.columns
-            
+
         # On parcourt les features
         for feature in features:
             if datas[feature].dtype == "object":
@@ -52,12 +91,27 @@ class datas_filter():
         
 
 if __name__ == "__main__":
-    datas_filter = datas_filter()
-    # Test downcast
+    filter = datas_filter()
+    # Exemple downcast
     datas = pd.DataFrame({"a": [1, 2, 3], "b": [1.0, 2.0, 3.0], "c": ["a", "b", "c"], 
                           "d": [1, 2.0, "c"], "e": [1, 2.0, 2], "f": [1, 2000, 3]})
     print("Avant downcast :")
     print(datas.dtypes)
-    datas = datas_filter.downcast(datas)
+    datas = filter.downcast(datas)
     print("Après downcast :")
     print(datas.dtypes)
+
+    # Exemple filter
+    # Add categorical features to datas
+    datas["g"] = pd.Categorical(["a", "b", "b"], ordered=False)
+    datas["h"] = pd.Categorical(["a", "a", "a"], ordered=False)
+    datas["i"] = pd.Categorical(["low", "medium", "high"], ordered=True)
+    print("Mise en place des filtres :")
+    print("Colones numériques : ")
+    print(list(filter.filter(datas, type="numerical").columns))
+    print("Colones ordinales : ")
+    print(list(filter.filter(datas, type="ordinal").columns))
+    print("Colones non-ordinales (max 2): ")
+    print(list(filter.filter(datas, type="non-ordinal", category_count=2).columns))
+    print("Colones catégorielles : ")
+    print(list(filter.filter(datas, type="categorical").columns))
