2dd1a5e6971c9089961b44a3479cda456beffd14
NansK <77019404+NansK@users.noreply.github.com>
2023-04-06 15:56:46 +0200

    Function to fit KMeans model (add asserts, set more parameters)

--- scripts/kmeans.py
+++ scripts/kmeans.py
@@ -1,4 +1,4 @@
-def kmeans(dataset, k=8):
+def kmeans(dataset, k=8, inp_algo='lloyd', inp_init='k-means++', inp_n_init=10):
     """
      This function will fit a KMeans model. You pass the OpenFoodFact dataset as entry and you get the fitted model at the end.
     You can precise the number of cluster expected.
@@ -6,6 +6,9 @@ def kmeans(dataset, k=8):
     Args :
         dataset -- the dataset who contains OpenFoodFact data (required)
         k -- number of clusters -- Default = 8
+        "inp_algo": K-means algorithm to use. Possible values are ['lloyd', 'elkan', 'auto', 'full'] -- Default = 'lloyd'
+        "inp_init": Method for initialization. Possible values are ["k-means++", "random"] -- Default = 'k-means++'
+        "inp_n_init": Number of times the k-means algorithm is run with different centroid seeds. Possible values are "auto" or an integer. -- Default = 10
 
     Returns :
         Fitted model of KMeans.
@@ -14,7 +17,7 @@ def kmeans(dataset, k=8):
     import numpy as np
     from numpy.testing import assert_equal
     from sklearn.cluster import KMeans
-    from sklearn.model_selection import GridSearchCV
+
     assert_equal(type(dataset), type(pd.DataFrame()), err_msg='Input is not Pandas Dataframe.', verbose=True)
     for i in dataset.columns:
         assert dataset[i].dtype in [type(5), type(5.5), np.int64().dtype, np.int32().dtype, np.float64().dtype,
@@ -24,11 +27,6 @@ def kmeans(dataset, k=8):
     assert_equal(dataset.isna().sum().sum(), 0,
                  err_msg=f'There is {dataset.isna().sum().sum()} NaN values in dataset, please preprocess them before '
                          f'trying to fit KMeans.')
-    model = KMeans(n_clusters=k, random_state=13, n_init=10)
-    gs_cv = GridSearchCV(model, cv=5, param_grid=[{"algorithm": ['lloyd', 'elkan', 'auto', 'full']},
-                                                  {"init": ["k-means++", "random"]},
-                                                  {"n_init": ["auto", 5, 10, 25, 50]}])
-    gs_cv.fit(dataset)
-    model = gs_cv.best_estimator_
+    model = KMeans(n_clusters=k, random_state=13, algorithm=inp_algo, init=inp_init, n_init=inp_n_init)
     assert_equal(type(model), type(KMeans()), err_msg='Output will not be KMeans class instance.', verbose=True)
     return model
