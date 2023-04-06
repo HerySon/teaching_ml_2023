c82e9575f97089341460c35492bf0d47644ef2ff
Santoudllo <122670817+Santoudllo@users.noreply.github.com>
2023-04-06 16:34:17 +0200

    Delete netoyer.py

--- netoyer.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-url= 'https://filedn.eu/lefeldrXcsSFgCcgc48eaLY/datasets/clusteringOFF/en.openfoodfacts.org.products.csv'
-df = pd.read_csv(url, sep='\t', low_memory=False)
-print(df.head(5))
