2019b91102a5f4a4d506db483692aa17bfe9b156
NansK <77019404+NansK@users.noreply.github.com>
2023-04-07 12:28:08 +0200

    Update rm_incoherent_values.py
    Add conditionals and arguments, and update docstrings

--- scripts/rm_incoherent_values.py
+++ scripts/rm_incoherent_values.py
@@ -1,10 +1,16 @@
-def incoherent_values(dataset):
+def incoherent_values(dataset, check_max_nrj = True, check_sug_carb = True):
     import pandas as pd
     """
     This function aim to clean incoherent values from features. You pass the OpenFoodFact dataset as entry and you get the processed dataset at the end.
 
     Args :
         dataset -- the dataset who contains OpenFoodFact data (required)
+        
+        check_max_nrj -- Boolean value to define if remooving the entries with energy for 100g superior to 3700 Kj. 
+        (can't be more than this treshold for 100g, the most rich aliments in term of Kj for 100g are oils)
+        
+        check_sug_carb -- Sugars are members of the carbohydrates family (they are simple carbohydrates), we can't have a value of carbohydrates who is inferior to sugars.
+        Boolean value to define if processing remooving of incoherent values (carbohydrates inferior to sugars) or not.
 
     Returns :
         Dataset of OpenFoodFact cleaned of some incoherent values.
@@ -20,7 +26,9 @@ def incoherent_values(dataset):
         if col not in ["energy_100g"]:
             nutrition_table = nutrition_table.loc[nutrition_table[col] <= 100]
         nutrition_table = nutrition_table.loc[nutrition_table[col] >= 0]
-    nutrition_table = nutrition_table.loc[nutrition_table.energy_100g <= 3700]
-    nutrition_table = nutrition_table.loc[nutrition_table.carbohydrates_100g >= nutrition_table.sugars_100g]
+    if check_max_nr == True:
+        nutrition_table = nutrition_table.loc[nutrition_table.energy_100g <= 3700]
+    if check_sug_carb == True:
+        nutrition_table = nutrition_table.loc[nutrition_table.carbohydrates_100g >= nutrition_table.sugars_100g]
     dataset = dataset.loc[nutrition_table.index, :]
     return dataset
