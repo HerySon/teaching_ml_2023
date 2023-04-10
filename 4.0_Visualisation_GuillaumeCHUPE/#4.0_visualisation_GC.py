4bed30aed0011710dc252d1de44deb4117b01a77
guillaumechupe <74655233+guillaumechupe@users.noreply.github.com>
2023-04-10 09:51:57 +0200

    V2 with correction & TODO

--- 4.0_Visualisation_GuillaumeCHUPE/#4.0_visualisation_GC.py
+++ 4.0_Visualisation_GuillaumeCHUPE/#4.0_visualisation_GC.py
@@ -1,78 +1,10 @@
-#import pandas as pd
-#import matplotlib.pyplot as plt
-#from wordcloud import WordCloud
-#import unittest
+import pandas as pd
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud
 
 
-def plot_wordcloud(data, column):
-    """
-    Displays a wordcloud for the given column.
-
-    Args:
-        data (DataFrame): The dataframe to use.
-        column (str): The name of the column to display.
-    """
-    text = ' '.join(data[column].astype(str).tolist())
-    wordcloud = WordCloud(width=800, height=800, background_color='white', colormap='Set2', min_font_size=10).generate(text)
-
-    plt.figure(figsize=(8, 8), facecolor=None)
-    plt.imshow(wordcloud)
-    plt.axis('off')
-    plt.tight_layout(pad=0)
-    plt.show()
-
-
-def plot_density(data, column):
-    """
-    Displays a density plot for the given column.
-
-    Args:
-        data (DataFrame): The dataframe to use.
-        column (str): The name of the column to display.
-    """
-    data[column].plot(kind='density')
-    plt.show()
-
-
-def plot_unique_values(data):
-    """
-    Displays a barplot for each column of the given dataframe,
-    showing the number of unique values for each column.
-
-    Args:
-        data (DataFrame): The dataframe to use.
-    """
-    unique_vals = []
-    for col in data.columns:
-        unique_vals.append(data[col].nunique())
-
-    plt.bar(data.columns, unique_vals)
-    plt.xticks(rotation=90)
-    plt.xlabel('Columns')
-    plt.ylabel('Number of unique values')
-    plt.show()
-
-
-def plot_histogram(data, column):
-    """
-    Displays a histogram for the given column.
-
-    Args:
-        data (DataFrame): The dataframe to use.
-        column (str): The name of the column to display.
-    """
-    data[column].hist()
-    plt.xlabel(column)
-    plt.ylabel('Frequency')
-    plt.show()
-
-    """
-    unit test for each fonctions bellow
-    """
-
-    class TestVisualizationFunctions(unittest.TestCase):
-
-      def setUp(self):
+class TestVisualizationFunctions:
+    def __init__(self):
         # Create a sample dataframe for testing
         self.df = pd.DataFrame({
             'col1': ['florent', 'joelle', 'nicolas', 'thomas'],
@@ -86,7 +18,7 @@ def plot_histogram(data, column):
         plot_wordcloud(self.df, 'col1')
 
     def test_plot_density(self):
-        # Test if the function ednsity runs without errors
+        # Test if the function density runs without errors
         plot_density(self.df, 'col2')
 
     def test_plot_unique_values(self):
@@ -96,3 +28,85 @@ def plot_histogram(data, column):
     def test_plot_histogram(self):
         # Test if the function histogram runs without errors
         plot_histogram(self.df, 'col4')
+
+
+def plot_wordcloud(data, column):
+    """
+    Displays a wordcloud for the given column.
+
+    Args:
+        data (DataFrame): The dataframe to use. Should contain at least one column of string type.
+        column (str): The name of the column to display. Should be a string column.
+    """
+    if isinstance(data[column].iloc[0], str):
+      text = ' '.join(data[column].astype(str).tolist())
+      wordcloud = WordCloud(width=800, height=800, background_color='white', colormap='Set2', min_font_size=10).generate(text)
+
+      plt.figure(figsize=(8, 8), facecolor=None)
+      plt.imshow(wordcloud)
+      plt.axis('off')
+      plt.tight_layout(pad=0)
+      plt.show()
+    else:
+      raise ValueError("Column should be of string type")
+
+
+def plot_density(data, column):
+    """
+    Displays a density plot for the given column.
+
+    Args:
+        data (DataFrame): The dataframe to use. Should contain at least one numeric column.
+        column (str): The name of the column to display. Should be a numeric column.
+    """
+    if pd.api.types.is_numeric_dtype(data[column]):
+      data[column].plot(kind='density')
+      plt.show()
+    else :
+      raise ValueError("Column should be of numeric type")
+
+
+def plot_unique_values(data):
+    """
+    Displays a barplot for each column of the given dataframe,
+    showing the number of unique values for each column.
+
+    Args:
+        data (DataFrame): The dataframe to use. Should contain at least one numeric column.
+    """
+    numeric_cols = data.select_dtypes(include=['number']).columns
+    if len(numeric_cols) == 0:
+        raise ValueError("At least one column should be of numeric type")
+    else:
+        for col in numeric_cols:
+            unique_vals = data[col].nunique()
+            plt.bar(col, unique_vals)
+        plt.xticks(rotation=90)
+        plt.xlabel('Columns')
+        plt.ylabel('Number of unique values')
+        plt.show()
+
+
+def plot_histogram(data, column):
+    """
+    Displays a histogram for the given column.
+
+    Args:
+        data (DataFrame): The dataframe to use. Should contain at least one numeric column.
+        column (str): The name of the column to display. Should be a numeric column.
+    """
+    if isinstance(data[column].iloc[0], (int, float)):
+      data[column].hist()
+      plt.xlabel(column)
+      plt.ylabel('Frequency')
+      plt.show()
+    else:
+      raise ValueError("Column should be of numeric type")
+
+if __name__ == "__main__":
+    # Launch the tests
+    test_visualization_functions = TestVisualizationFunctions()
+    test_visualization_functions.test_plot_wordcloud()
+    test_visualization_functions.test_plot_density()
+    test_visualization_functions.test_plot_unique_values()
+    test_visualization_functions.test_plot_histogram()
