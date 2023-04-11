b00f30dee05639e1883b22eb60aa1f547d0e8967
guillaumechupe <74655233+guillaumechupe@users.noreply.github.com>
2023-04-11 19:24:05 +0200

    Correction majeures faites

--- #0.0_Filtrage_GuillaumeCHUPE.py
+++ #0.0_Filtrage_GuillaumeCHUPE.py
@@ -1,4 +1,6 @@
-#0.0_Filtrage_GuillaumeCHUPE
+import pandas as pd
+import numpy as np
+from sklearn.impute import KNNImputer
 
 def filter_dataframe(df, num_cols=None, ordinal_cols=None, nominal_cols=None, downcast_cols=None, max_categories=None):
     """
@@ -6,37 +8,41 @@ def filter_dataframe(df, num_cols=None, ordinal_cols=None, nominal_cols=None, do
 
     Args:
         df (DataFrame): The input dataframe to filter.
-        num_cols (list of str): List of column names for numeric columns.
-        ordinal_cols (list of str): List of column names for ordinal columns.
-        nominal_cols (list of str): List of column names for nominal columns.
-        downcast_cols (list of str): List of column names for columns that need to be downcasted.
-        max_categories (int): Maximum number of categories allowed for nominal columns. If None, all nominal columns are kept.
+        num_cols (list of str): List of column names for numeric columns. Default is None.
+        ordinal_cols (list of str): List of column names for ordinal columns. Default is None.
+        nominal_cols (list of str): List of column names for nominal columns. Default is None.
+        downcast_cols (list of str): List of column names for columns that need to be downcasted. Default is None.
+        max_categories (int): Maximum number of categories allowed for nominal columns. If None, all nominal columns are kept. Default is None.
 
     Returns:
         DataFrame: The filtered dataframe.
     """
-    # initialize empty lists for each column type
-    numeric_cols = []
-    ordinal_cols = [] if ordinal_cols else []
-    nominal_cols = [] if nominal_cols else []
-    downcast_cols = [] if downcast_cols else []
+    # Filter numeric columns
+    if num_cols:
+        numeric_cols = df.select_dtypes(include=['number']).columns.intersection(num_cols)
+    else:
+        numeric_cols = df.select_dtypes(include=['number']).columns
 
-    # iterate through all columns
-    for col in df.columns:
-        if df[col].dtype in [np.int8, np.int16, np.int32, np.int64, np.float16, np.float32, np.float64]:
-            # numeric column
-            numeric_cols.append(col)
-        elif ordinal_cols and col in ordinal_cols:
-            # ordinal column
-            ordinal_cols.append(col)
-        elif nominal_cols and col in nominal_cols:
-            # nominal column
-            if max_categories is None or df[col].nunique() <= max_categories:
-                nominal_cols.append(col)
-        if downcast_cols and col in downcast_cols:
-            # downcast column
+    # Filter ordinal columns
+    if ordinal_cols:
+        ordinal_cols = df[ordinal_cols].columns.intersection(ordinal_cols)
+    else:
+        ordinal_cols = []
+
+    # Filter nominal columns
+    if nominal_cols:
+        if max_categories is not None:
+            nominal_cols = df[nominal_cols].columns[df[nominal_cols].nunique() <= max_categories]
+        else:
+            nominal_cols = df[nominal_cols].columns
+    else:
+        nominal_cols = []
+
+    # Downcast columns
+    if downcast_cols:
+        for col in downcast_cols:
             df[col] = pd.to_numeric(df[col], downcast='integer')
 
-    # return filtered dataframe with selected columns
-    selected_cols = numeric_cols + ordinal_cols + nominal_cols
+    # Return filtered dataframe with selected columns
+    selected_cols = list(numeric_cols) + list(ordinal_cols) + list(nominal_cols)
     return df[selected_cols]
