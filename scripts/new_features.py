2bf52b6a9dd05c8dc86e0901b34f46af8cf4cd46
Joelleuh <joelle.sabourdy@ynov.com>
2023-04-11 21:48:44 +0200

    Tache8.3_Joelle

---
+++ scripts/new_features.py
@@ -0,0 +1,64 @@
+import pandas as pd
+import numpy as np
+
+def split_ean13(df, drop_ean13=False):
+    """
+    This function split code column into 3 separate columns if it is digit and its length is 13.
+    Code column is barcode of the product (can be EAN-13 or internal codes for some food stores).
+    Position 1 to 3 : country code
+    Position 3 to 7 : manufacturer code
+    Position 8 to 12 : product code
+    Position 13 : check digit that will be removed
+    Parameters:
+    - df : pandas dataframe (The input dataframe containing the EAN13 codes in a column named "code")
+    - drop_ean13 : bool (Default=False), drop code column if set True
+    Returns:
+    - df : pandas dataframe with the separated EAN13 columns
+    """
+    # Check if code column exists
+    if 'code' not in df.columns:
+        raise ValueError('code column not found in dataframe.')
+    
+    # Create new columns
+    df['EAN13_country'] = np.nan
+    df['EAN13_company'] = np.nan
+    df['EAN13_product'] = np.nan
+    
+    # Loop over rows
+    for i, row in df.iterrows():
+        ean = str(row['code'])
+        # Check if code is valid with EAN13
+        if not ean.isdigit():
+            continue
+        # Check code length
+        if len(ean) != 13:
+            continue
+        # Split EAN13
+        prefix = ean[:3]
+        company = ean[2:7]
+        product = ean[7:12]
+        # Store values
+        df.at[i, 'EAN13_country'] = prefix
+        df.at[i, 'EAN13_company'] = company
+        df.at[i, 'EAN13_product'] = product
+    
+    # Drop code column if specified
+    if drop_ean13:
+        df.drop('code', axis=1, inplace=True)
+    
+    return df
+
+
+
+
+# Test
+
+# Consider dataset containing ramen product
+df = pd.DataFrame({
+    'brand': ['Yum Yum', 'Yum Yum', 'Indomie', 'Indomie', 'Tanoshi', 'Cup Noodles'],
+    'style': ['cup', 'cup', 'cup', 'pack', 'pack', 'cup'],
+    'code': ['1234567890123', '123456', '4567890123456', '', '0067890123456', '1234567890az3456']
+    })
+
+# Split code column
+split_ean13(df, drop_ean13=False)
