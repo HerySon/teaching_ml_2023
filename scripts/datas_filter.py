7c1c24ec3e6d020440609c457d4bcf10d46e52f7
Yonuko <sacha.eghiazarian@ynov.com>
2023-04-11 22:24:27 +0200

    Translate every comment in english (so we do for the commit =D)

--- scripts/datas_filter.py
+++ scripts/datas_filter.py
@@ -16,21 +16,21 @@ class datas_filter():
             return : str
                 Type de données le plus adapté
         """
-        # On vérifie que les valeurs en entrée soient du bon type
+        # We check that the minvalue and maxvalue are int64
         assert isinstance(minvalue, np.int64), "minvalue doit être un int64"
         assert isinstance(maxvalue, np.int64), "maxvalue doit être un int64"
 
-        # On parcourt les types de données
+        # Loop through datatypes
         for dtype in self.dtypes_list:
-            # On vérifie que la valeur minimale soit supérieure à 0 pour les types de données unsigned
+            # Check that minvalue is greater than 0 for unsigned type 
             if minvalue > 0 and "u" not in dtype:
                 continue
-            # On vérifie que la valeur minimale et maximale soient dans la range du type de données
+            # Check if min and max value are in the range of the current type
             if minvalue >= np.iinfo(dtype).min and maxvalue <= np.iinfo(dtype).max:
                 # On retourne le type de données
                 return dtype
         else:
-            # On retourne "notFound" si aucun type de données n'est trouvé
+            # Return notFound if the type was not found
             return "notFound"
 
 
@@ -57,34 +57,34 @@ class datas_filter():
             return : pd.DataFrame
                 Données filtrées
         """
-        # On vérifie que les données en entrée soient du bon type
+        # Check the type of the input datas
         assert isinstance(datas, pd.DataFrame), "datas doit être un pd.DataFrame"
         assert isinstance(type, str), "type doit être un str"
         assert isinstance(category_count, int), "category_count doit être un int"
         assert isinstance(nan_percent, int) or nan_percent >= 0 or nan_percent <= 100, "nan_percent doit être un int entre 0 et 100"
         assert isinstance(ordinal_features_names, list) or ordinal_features_names is None, "ordinal_features_names doit être une liste"
 
-        # On vérifie que le type soit valide
+        # Check that type is available
         assert type in ["number", "ordinal", "non-ordinal", "categorical"], "type doit être dans ['number', 'ordinal', 'non-ordinal', 'categorical']"
 
-        # On vérifie que le nombre de catégories soit valide
+        # We check that the category_count is valid
         assert category_count > 0, "category_count doit être supérieur à 0"
 
         if type == "number":
-            # On récupère les features numériques
+            # We get the numerical features
             features = datas.select_dtypes(include=["integer", "float"]).columns
         elif type == "ordinal":
             if ordinal_features_names is None:        
-                # On retourne le dataframe vide
+                # If there is no ordinal features selected, we return an empty dataframe
                 return pd.DataFrame()
-            # On récupère les features catégorielles ordinales
+            # We get the ordinal features
             features = ordinal_features_names
         elif type == "non-ordinal" or type == "categorical":
-            # On récupère les features catégorielles non-ordinales
+            # We get the categorical features
             features = datas.select_dtypes(include=["category"]).columns
             if type == "non-ordinal":
                 for feature in features:
-                    # On vérifie que la feature ne soit pas ordonnée et qu'elle ne contienne pas trop de catégories
+                    # We check if the feature is ordinal and doesn't contains too many categories
                     if datas[feature].cat.ordered or datas[feature].nunique() > category_count:
                         features = features.drop(feature)
         result_df = datas[features]
@@ -104,39 +104,39 @@ class datas_filter():
             return : pd.DataFrame
                 Données réduites
         """
-        # On vérifie que les données en entrée soient du bon type
+        # Check the type of the input datas
         assert isinstance(datas, pd.DataFrame), "datas doit être un pd.DataFrame"
         assert isinstance(features, list) or features is None, "features doit être une liste"
         assert isinstance(show_saved_memory, bool), "show_saved_memory doit être un bool"
 
         if show_saved_memory:
-            # On sauvegarde la taille mémoire des données avant filtrage
+            # Save the memory size before the reduction
             before = datas.memory_usage(index=False, deep=True).sum()
 
-        # Si aucune feature n'est spécifiée, on prend toutes les features
+        # If no features are selected, we select all the features
         if features is None:
             features = datas.columns
 
-        # On parcourt les features
+        # Loop through the features
         for feature in features:
             if datas[feature].dtype == "object":
-                # On verifie que la feature ne soit pas multitype
+                # Check that the feature is not a mix of types
                 if datas[feature].apply(type).nunique() > 1:
                     continue
-                # Si la feature n'est que du type string, on la réduit
+                # If the feature is only composed of strings, we convert it to category
                 datas[feature] = datas[feature].astype("category")
                 continue
             elif datas[feature].dtype == "int64":
-                # On récupère le type de données le plus adapté
+                # Get the most appropriate type
                 dtype = self._get_type_by_range(datas[feature].min(), datas[feature].max())
                 if dtype == "notFound":
                     continue
-                # On réduit la feature
+                # Reduce the feature
                 datas[feature] = datas[feature].astype(dtype)
             elif datas[feature].dtype == "float64":
                 datas[feature] = datas[feature].astype("float16")
         if show_saved_memory:
-            # On sauvegarde la taille mémoire des données après filtrage
+            # We save the memory size after the reduction and we display the difference
             after = datas.memory_usage(index=False, deep=True).sum()
             print(f"Taille mémoire des données avant filtrage : {before} octets")
             print(f"Taille mémoire des données après filtrage : {after} octets")
@@ -146,13 +146,13 @@ class datas_filter():
 
 if __name__ == "__main__":
     filter = datas_filter()
-    # Exemple downcast
+    # downcast exemple
     datas = pd.DataFrame({"a": [1, 2, 3], "b": [1.0, 2.0, 3.0], "c": ["a", "b", "c"], 
                           "d": [1, 2.0, "c"], "e": [1, 2.0, 2], "f": [1, 2000, np.nan],
                           "g": ["a", "b", "b"], "h": ["a", "a", "a"], "i": ["low", "medium", "high"]})
     datas = filter.downcast(datas, show_saved_memory=True)
 
-    # Exemple filter
+    # filter exemples
     print("Mise en place des filtres :")
     print("Colones numériques : ")
     print(list(filter.filter(datas, type="number", nan_percent=34).columns))
