b582cf875de57e128f6e964312a89d4de930eaed
guillaumechupe <74655233+guillaumechupe@users.noreply.github.com>
2023-04-12 19:58:43 +0200

    Corrections mineures

--- #1.1_EncoderValeursNonNumeriques_GuillaumeCHUPE.py
+++ #1.1_EncoderValeursNonNumeriques_GuillaumeCHUPE.py
@@ -1,5 +1,11 @@
 #1.1_EncoderFeaturesNONNumériques_GuillaumeCHUPE
 
+import pandas as pd
+from sklearn.preprocessing import OrdinalEncoder, OneHotEncoder
+from category_encoders.hashing import HashingEncoder
+from category_encoders.count import CountEncoder
+from scipy.sparse import csr_matrix
+
 def encode_ordinal(data, cols):
     """
     Encodes the columns specified in cols using ordinal encoding.
@@ -16,33 +22,37 @@ def encode_ordinal(data, cols):
     return data
 
 
-def encode_onehot(data, cols):
+def encode_onehot(data, cols, sparse_output=True):
     """
     Encodes the columns specified in cols using one-hot encoding.
 
     Parameters:
     data (DataFrame): The dataframe containing the columns to encode.
     cols (list): The list of column names to encode.
+    sparse_output (bool): Whether to return a sparse matrix. Default is True.
 
     Returns:
-    DataFrame: The dataframe with encoded columns.
+    DataFrame or csr_matrix: The dataframe with encoded columns in sparse or dense format.
     """
-    encoder = OneHotEncoder()
+    encoder = OneHotEncoder(sparse=sparse_output)
     encoded_cols = encoder.fit_transform(data[cols])
-    encoded_data = pd.DataFrame(encoded_cols.toarray(), columns=encoder.get_feature_names(cols))
+    if sparse_output:
+        encoded_data = pd.DataFrame(encoded_cols)
+    else:
+        encoded_data = pd.DataFrame(encoded_cols.toarray(), columns=encoder.get_feature_names_out(cols))
     data = data.drop(columns=cols)
     data = pd.concat([data, encoded_data], axis=1)
     return data
 
 
-def encode_hash(data, cols, n_components):
+def encode_hash(data, cols, n_components=8):
     """
     Encodes the columns specified in cols using hashing encoding.
 
     Parameters:
     data (DataFrame): The dataframe containing the columns to encode.
     cols (list): The list of column names to encode.
-    n_components (int): The number of dimensions in the encoding.
+    n_components (int): The number of dimensions in the encoding. Default is 8.
 
     Returns:
     DataFrame: The dataframe with encoded columns.
