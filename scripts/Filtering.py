3cc5d660eed4b2ec5ceaeed494e3e9bea8a0f96c
Thomas Payan <thomas.payan@ynov.com>
2023-04-12 23:32:07 +0200

    Preshot minor corrections

--- scripts/Filtering.py
+++ scripts/Filtering.py
@@ -2,44 +2,62 @@ from data_loader import *
 import pandas as pd
 
 class Filtering:
-
-    d_df        = None
-    d_endswith  = ["_t","_datetime","_url"]
-    d_wendswith = ["_tags"]
+    """Class to filtering pandas dataframe
+    Args:
+        df (DataFrame): pandas dataframe
+        endswith (array): list of features ending with
+        wendswith (array): list (invert) of features ending with
+    Returns:
+        df (DataFrame): return filtered pandas dataframe
+    @Author: Thomas PAYAN
+    """
 
     def __init__(
                     self,
-                    df        = d_df,
-                    endswith  = d_endswith,
-                    wendswith = d_wendswith
+                    df        = None,
+                    endswith  = ["_t","_datetime","_url"],
+                    wendswith = ["_tags"]
                 ):
 
-        # Init attributes
         self.df        = df
         self.endswith  = endswith
         self.wendswith = wendswith
-
-    def get_dataset_len(self):
-        return len(self.df.columns)
         
-    # Functions to get numerical features
     def get_numerical_features(self):
-        df_num = self.df.select_dtypes(exclude=["object"])
+        """Get numerical features
+        Returns:
+            df_num (DataFrame): return numerical features pandas dataframe
+        @Author: Thomas PAYAN
+        """
+        df_num = self.df.select_dtypes(include=["number"])
         print("\nNumerical features list :")
         for feature in df_num.columns.tolist(): print(feature)
         return df_num
 
-    # Functions to get categorical features
     def get_categorical_features(self):
+        """Get categorical features
+        Returns:
+            df_cat (DataFrame): return categorical features pandas dataframe
+        @Author: Thomas PAYAN
+        """
         df_cat = self.df.select_dtypes(include=["object"])
         print("\nCategorical features list :")
         for feature in df_cat.columns.tolist(): print(feature)
         return df_cat
     
     def get_features_endswith(self, endswith, invert=False):
+        """Get features endswith method
+        Args:
+            endswith (array) : list of features ending with
+            invert (boolean) : to get features without the end of tag
+        Returns:
+            df_endswith (DataFrame): return selected features pandas dataframe
+        @Author: Thomas PAYAN
+        """
         print("\nGet features endswith")
         ft_endswith = []
         ft_list     = self.df.columns
+        
         for feature in ft_list.tolist():
             for end in endswith:
                 if feature.endswith(end):
@@ -53,6 +71,13 @@ class Filtering:
         return df_endswith
     
     def drop_features(self, features):
+        """Drop features list
+        Args:
+            features (Dataframe) : features to drop
+        Returns:
+            df (DataFrame): return droped features pandas dataframe
+        @Author: Thomas PAYAN
+        """
         print("\nDrop features processing...")
         for feature in features:
             print("Deleting feature : "+feature)
@@ -60,22 +85,20 @@ class Filtering:
     
     # Function to preprocess the Dataframe
     def filtering(self):
-
+        """Filter dataframe
+        Returns:
+            df (DataFrame): return filtered pandas dataframe
+        @Author: Thomas PAYAN
+        """
         self.get_numerical_features()
 
         self.get_categorical_features()
 
         ft_endswith = self.get_features_endswith(endswith=self.endswith)
-        # print(ft_endswith.head())
-
         self.drop_features(ft_endswith)
 
         ft_wendswith = self.get_features_endswith(endswith=self.wendswith, invert=True)
-        # print(ft_wendswith.head())
-
         self.drop_features(ft_wendswith)
-
-        print(self.df[['packaging_tags']].head(50))
     
         return self.df
         
