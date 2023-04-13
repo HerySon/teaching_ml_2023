ac65f1d02b5903674aefc83bb11ac652830ab0f2
keagnon <keagnon.gbe@gmail.com>
2023-04-14 00:20:20 +0200

    Modification effectué pour la tache 3 gestion des valeurs abbérentes : Implémentation de certain librairies tel que DBSCAN,IsolationForest

--- scripts/gerer_valeurs_ab.py
+++ scripts/gerer_valeurs_ab.py
@@ -1,53 +1,80 @@
-import pandas as pd
 import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.cluster import DBSCAN
+from sklearn.ensemble import IsolationForest
 
-df = pd.read_csv(r"C:\Users\Fnac\Desktop\Trello ML\Trello_git/teaching_ml_2023/dataset_clean.csv", encoding='utf-8')
-
-
-def gestion_valeurs_aberrantes(df, colonnes=None, seuil=3, action="delete"):
-    
+def manage_outliers(data, method='StandardScaler', threshold=3, strategy='median'):
     """
-        ------------------------------------------------------------------------------------------------------------------------------------
-        Objectif : 
-        ------------
-        Cette fonction prend en entrée un dataframe, une liste de colonnes, un seuil et une action à effectuer en cas de valeurs aberrantes.
-        Elle renvoie un dataframe modifié selon l'action choisie.
-        --------------
-        Paramètres
-        --------------
-        df: dataframe contenant les données
-        colonnes: liste des noms des colonnes à traiter (par défaut, toutes les colonnes sont traitées)
-        seuil: seuil utilisé pour déterminer les valeurs aberrantes (par défaut, seuil = 3)
-        action: action à effectuer en cas de valeurs aberrantes ("delete" pour supprimer, "mean" pour remplacer par la moyenne)
-        ---------------
-        En sortie: dataframe modifié selon l'action choisie
-        -------------------------------------------------------------------------------------------------------------------------------------
-        
+    ---------------------------------------------------------------
+    Goal:
+    Detects and manages outliers in a dataset using various methods.
+    ---------------------------------------------------------------
+    Parameters:
+    -----------
+    data : pandas.DataFrame or numpy.ndarray
+        The dataset to manage outliers for.
+    method : str, optional (default='StandardScaler')
+        The method to use for outlier detection. Can be one of:
+            - 'StandardScaler': Uses the StandardScaler method to scale the data and 
+               then uses the z-score method based on the standard deviation.
+            - 'DBSCAN': Uses the DBSCAN clustering algorithm to identify outliers.
+            - 'IsolationForest': Uses the Isolation Forest algorithm to identify outliers.
+    threshold : float, optional (default=3)
+        The threshold used to determine outliers. 
+        Values above or below this threshold are considered outliers.
+    strategy : str, optional (default='median')
+        The strategy used to manage outliers. Can be one of:
+            - 'median': Replaces outliers with the median value of the feature.
+            - 'mean': Replaces outliers with the mean value of the feature.
+    ----------------------------------------------------------------------
+    Returns:
+    --------
+    pandas.DataFrame
+        The cleaned dataset with outliers managed.
     """
-    # Si aucune colonne n'est spécifiée, on traite toutes les colonnes
-    if colonnes is None:
-        colonnes = df.columns
-    
-    # Calcule de la moyenne et de l'écart-type pour chaque colonne spécifiée
-    moyennes = np.mean(df[colonnes], axis=0)
-    ecarts_types = np.std(df[colonnes], axis=0)
-    
-    # Détermination des valeurs aberrantes pour chaque colonne spécifiée
-    valeurs_aberrantes = (df[colonnes] < moyennes - seuil * ecarts_types) | (df[colonnes] > moyennes + seuil * ecarts_types)
-    
-    # Suppression ou remplacement des lignes contenant des valeurs aberrantes
-    if action == "delete":
-        df_modifie = df[~np.any(valeurs_aberrantes, axis=1)]
-    elif action == "mean":
-        df_modifie = df.copy()
-        for c in colonnes:
-            df_modifie[c] = np.where(valeurs_aberrantes[c], moyennes[c], df_modifie[c])
-    else:
-        raise ValueError("L'action choisie n'est pas valide. Veuillez choisir 'delete' ou 'mean'.")
-    
-    return df_modifie
+    # Scaling the data if method is StandardScaler
+    if method == 'StandardScaler':
+        scaler = StandardScaler()
+        data = scaler.fit_transform(data)
 
-# Sélection des variables numériques dont nous souhaitons gérer les valeurs abbérantes
-colonnes_float = df.select_dtypes(include=['float']).columns
-df_sans_aberrantes = gestion_valeurs_aberrantes(df,colonnes_float,seuil=3, action="delete")
-df_sans_aberrantes
+    # Detecting outliers using the selected method
+    if method == 'StandardScaler':
+        z_scores = np.abs(data)
+        outliers = np.where(z_scores > threshold)
+    elif method == 'DBSCAN':
+        dbscan = DBSCAN(eps=threshold, min_samples=2)
+        outliers = dbscan.fit_predict(data) == -1
+    elif method == 'IsolationForest':
+        iso_forest = IsolationForest(contamination=threshold)
+        outliers = iso_forest.fit_predict(data) == -1
+
+    # Managing outliers using the selected strategy
+    if strategy == 'median':
+        replacements = np.median(data, axis=0)
+    elif strategy == 'mean':
+        replacements = np.mean(data, axis=0)
+
+    data[outliers] = replacements
+
+    # Returning the cleaned data
+    if method == 'StandardScaler':
+        data = scaler.inverse_transform(data)
+    return pd.DataFrame(data)
+
+
+if __name__ == "__main__":
+    
+    # Creating a simple dataframe with some outliers
+    df = pd.DataFrame({
+        'A': [1, 2, 3, 4, 5, 100],
+        'B': [2, 4, 6, 8, 10, 200],
+        'C': [5, 10, 15, 20, 25, 1000]
+    })
+
+    # Using the manage_outliers() function to clean the dataframe
+    clean_df = manage_outliers(df, method='IsolationForest', threshold=0.05, strategy='mean')
+
+    # Printing the original dataframe and the cleaned dataframe
+    print("Original Dataframe:\n", df)
+    print("\nCleaned Dataframe:\n", clean_df)
