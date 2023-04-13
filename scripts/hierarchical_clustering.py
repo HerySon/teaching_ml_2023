1031546cc99a34d1f56d9cceed1e22df1dcc4d83
Joelleuh <joelle.sabourdy@ynov.com>
2023-04-13 13:17:17 +0200

    change structure of the docstring

--- scripts/hierarchical_clustering.py
+++ scripts/hierarchical_clustering.py
@@ -7,17 +7,21 @@ from scipy.cluster.hierarchy import dendrogram
 
 def plot_dendrogram(model, **kwargs):
     """
-    This function creates a linkage matrix from the model and uses it to plot a dendrogram 
-    of the clustering. The linkage matrix is created by counting the number of samples under
-    each node of the dendrogram, and the scipy dendrogram function is used to actually plot
-    the dendrogram. Additional keyword arguments can be passed to the scipy dendrogram function
-    through **kwargs.
-    Args:
-        model : AgglomerativeClustering or FeatureAgglomeration object
+    This function creates a linkage matrix from the model and uses it to plot a
+    dendrogram of the clustering. The linkage matrix is created by counting the
+    number of samples under each node of the dendrogram, and the scipy dendrogram
+    function is used to actually plot the dendrogram. Additional keyword arguments
+    can be passed to the scipy dendrogram function through **kwargs.
+
+    Keyword arguments:
+    ------------------
+        model: AgglomerativeClustering or FeatureAgglomeration object
             The fitted hierarchical clustering model to visualize.
-        **kwargs : optional keyword arguments
+        **kwargs: optional keyword arguments
             Additional arguments to pass to the scipy dendrogram function.
+
     Returns:
+    --------
         None
     """
     # create the counts of samples under each node
@@ -40,29 +44,44 @@ def plot_dendrogram(model, **kwargs):
     dendrogram(linkage_matrix, **kwargs)
 
 
+
 def hie_clustering(df, modeltype=AgglomerativeClustering, n_clusters=2, linkage='ward', metric='euclidean', plotdendrogram=False):
     """
     Hierachical clustering method from Scikit-Learn.
-    Args:
-        df : pandas dataframe (The input dataframe)
-        modeltype : which clustering algorithm to use (default=AgglomerativeClustering)
-            'AgglomerativeClustering' : performs a hierarchical clustering using a bottom up approach: each observation starts in its own cluster, and clusters are successively merged together.
-            'FeatureAgglomeration' : uses agglomerative clustering to group together features that look very similar, thus decreasing the number of features.
-        n_clusters : number of cluster to find (default=2)
-        link : Which linkage criterion to use. The linkage criterion determines which distance to use between sets of features.
-            The algorithm will merge the pairs of cluster that minimize this criterion (default='ward')
-                'ward' minimizes the variance of the clusters being merged
-                'average' uses the average of the distances of each observation of the two sets
-                'complete' linkage uses the maximum distances between all observations of the two sets
-                'single' uses the minimum of the distances between all observations of the two sets
-        metric : Metric used to compute the linkage (default='euclidean')
+
+    Keyword arguments:
+    ------------------
+        df: pandas.DataFrame
+            The input DataFrame.
+        modeltype: type, optional (default AgglomerativeClustering)
+            Which clustering algorithm to use. Possible values are:
+            - AgglomerativeClustering : performs a hierarchical clustering using a bottom up approach:
+            each observation starts in its own cluster, and clusters are successively merged together.
+            - FeatureAgglomeration : uses agglomerative clustering to group together features that look
+            very similar, thus decreasing the number of features.
+        n_clusters: int, optional (default 2)
+            Number of cluster to find.
+        linkage: str, optional (default 'ward')
+            Which linkage criterion to use. The linkage criterion determines which distance to use
+            between sets of features. The algorithm will merge the pairs of cluster that minimize
+            this criterion. Possible values are:
+            - 'ward' minimizes the variance of the clusters being merged
+            - 'average' uses the average of the distances of each observation of the two sets
+            - 'complete' linkage uses the maximum distances between all observations of the two sets
+            - 'single' uses the minimum of the distances between all observations of the two sets
+        metric: str, optional (default 'euclidean')
+            Metric used to compute the linkage.
             Can be 'euclidean', 'l1', 'l2', 'manhattan', 'cosine', or 'precomputed'.
             If linkage is 'ward', only 'euclidean' is accepted.
-        plotdendrogram : bool (Default=True)
-            Will plot the dendogram
-    Return:
+        plotdendrogram: bool, optional (default False)
+            Will plot the dendogram.
+
+    Returns:
+    --------
         sklearn cluster object
+
     Author:
+    -------
         Joëlle Sabourdy
     """
     if linkage == 'ward' and metric != 'euclidean':
