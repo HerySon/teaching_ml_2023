6d42fcf7800703d6f3a7e55ac1de61fac174e30d
John Doe <florentsanchez13@gmail.com>
2023-04-13 11:23:23 +0200

    update

--- scripts/categorical_correlation.py
+++ scripts/categorical_correlation.py
@@ -13,52 +13,57 @@ def cross_table (df,index,columns) :
     Explore variance of the categorial features in a pandas dataframe using pandas.
     Args:
         df : pandas dataframe
-        index : str (column names to plot)
-        columns : list of str (The list of column names to plot)
+        index : str (column name to plot)
+        columns : str (column to plot)
 
     Returns : 
         Cross table
     """
     #Cross tables
-    data_crosstab = pd.crosstab(df.index, df.columns.,
+    data_crosstab = pd.crosstab(df.index, df.columns,
                             margins = False)
     return print(data_crosstab)
 
 
-def chi_2 (df,columns,alpha):
+def categorial_feature_selection(k,X, y):
     """
-    Explore independance of two categorial features in a pandas dataframe using pandas.
-    Args:
-        df : pandas dataframe
-        columns : list of str (The list of column names to plot)
-        alpha : 
-
-    Returns : 
-        Result of chi2
-    """
-    #Select categorial values
-    cat_val = df.select_dtypes(include=['object'])
-    #Clear values
-    observation = cat_val[[columns]].value_counts().dropna()
-    #Chi2
-    chi_squared_result=chi2_contingency(observation)
+    This score can be used to select the n_features features with the highest values.
+    must contain only non-negative features.
     
-    return print("Pearson stats of chi2 : ", chi_squared_result[0], "\n"
-    "La p-valeur du test est : ", chi_squared_result[1], "\n")
-    print("Le quantile de la loi du chi 2 à 1 degré de liberté associé au niveau de confiance",
-    alpha,"est",chi2.ppf(1-alpha, 1))
-  
+    Arg:
+    k(int): top features to be selected
+    X: {array-like, sparse matrix} of shape (n_samples, n_features)
+    y: array-like of shape (n_samples)
+    
+    Return
+    
 
-  def plot_chi2 (chi2_result):
-    """
-    Plot variance of two categorial features in a pandas dataframe using Maplotlib.
-    Args:
-        chi2_result : result of chi2 test
-   
-    Returns : 
-        Plot chi2
     """
 
-    p_values = pd.Series(chi2_result[1])
-    p_values.sort_values(ascending = False , inplace = True)
-    return p_values.plot.bar()
+    # k tells top features to be selected
+    # Score function Chi2 tells the feature to be selected using Chi Square
+    feature_score = SelectKBest(score_func=chi2, k=k)
+    fit = model.fit(X, y)
+    scores = fit.scores_
+    X_select = model.fit_transform(X, y)
+    return scores_,X_select
+
+def plot_univariate_feature_selection(X,scores,width):
+    """
+    Select the most significant features.
+    
+    Arg: X: {array-like, sparse matrix}
+        scores: array-like of shape (n_features,)
+        width (float)
+    
+    Return: plot 
+    """
+    X_indices = np.arange(X.shape[-1])
+    score = fit.scores_
+    plt.figure(1)
+    plt.clf()
+    plt.bar(X_indices, scores, width=width)
+    plt.title("Feature univariate score")
+    plt.xlabel("Feature number")
+    plt.ylabel("Univariate score")
+    return plt.show()
