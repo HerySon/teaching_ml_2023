2d4fe6821b2a24e6794e2f9cf3b48cf0dc918f57
santoudllo <mamadoualimou.diallo@ynov.com>
2023-04-13 11:07:56 +0200

    modification apportée

--- scripts/scaling.py
+++ scripts/scaling.py
@@ -1,26 +1,38 @@
-#utilisation des techniques de Scikit-Learn : 
-#normaliser le dataset deja nettoye par la methode de  Scikit-Learn  MinMaxScaler.
-# definir  min max scaler
-from numpy import asarray
-from sklearn.preprocessing import MinMaxScaler
-import pandas as pd
-import numpy as np
-# charger le dataset 
-data= pd.read_csv ()
-#En utilisant RobustScaler(), pour  pouvons supprimer les valeurs aberrantes
-scaler = RobustScaler()
-# standardiser le  jeu de données à l’aide de l’objet scikit-learn StandardScaler.
 from sklearn.preprocessing import StandardScaler
-# definir  standard scaler
-scaler = StandardScaler()
-# transformer  data
-scaled = scaler.fit_transform(data)
-print(scaled)
-# a la sortie le scaler cree  une version transformée du jeu de données avec chaque colonne standardisée indépendamment. 
-# definir MinMaxScaler
-scaler = MinMaxScaler()
-# transform er data
-scaled = scaler.fit_transform(data)
-print(scaled)
-# a la sortie le scaler s'adapte à l'ensemble du jeu de données et cree une transdormée  avec chaque colonne normalisée
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import MaxAbsScaler
+from sklearn.preprocessing import RobustScaler 
+#define functions used
+def scaling_data(df, scaler_name):
+   # la fonction permet à l'utilsateur de choisir sa methode ( MinMaxScaler, StandardScaler,StandardScaler) pour faire le scaling du data framme
+
+# scaler_name : la methode à utilisée 
+# ctake only variables we will experiment
+ features = ['recuperer les colonnes float ']
+df = df[features]
+#split data
+X_train, X_test, y_train, y_test = train_test_split(df[features],test_size=0.3,random_state=0)
+# choix de la methode 
+if scaler_name='StandardScaler' :
+   #fit the scaler to the train set 
+scaler_std = StandardScaler().fit(X_train)
+
+# transform data
+X_train_scaled_std = scaler_std.transform(X_train)
+else if scaler_name ='MinMaxScaler' :
+ # fit the scaler to the train set 
+scaler_minmax = MinMaxScaler().fit(X_train)
+
+# transform data
+X_train_scaled_minmax = scaler_minmax.transform(X_train)
+else if scaler_name='MaxAbsScaler' :
+scaler_maxabs = MaxAbsScaler().fit(X_train)
+ else if scaler_name='RobustScaler' :
+scaler_rbs = RobustScaler().fit(X_train)
+X_train_scaled_rbs = scaler_rbs.transform(X_train)
+else : 
+ print('saisir une methode')
+return rien 
+
+
 
