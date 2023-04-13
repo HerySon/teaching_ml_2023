6cc6307f87493eb404d1b8130d056861fa0d2e05
Massamba <m.massamba.gueye@gmail.com>
2023-04-13 10:39:29 +0200

    Reduire le svariables par l'UMAP

--- scripts/Reduction de variables avec UMAP.py
+++ scripts/Reduction de variables avec UMAP.py
@@ -58,7 +58,7 @@ pipeline = Pipeline([
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(df.drop(columns=['target']), df['target'], test_size=0.2, random_state=42)
 
-# Train and evaluate 
+# Train and evaluate the model
 pipeline.fit(X_train, y_train)
 accuracy = pipeline.score(X_test, y_test)
 print(f"Accuracy : {accuracy}")
