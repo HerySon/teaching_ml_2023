bdd8be5ddac258d2007562694d29d5990000f781
santoudllo <mamadoualimou.diallo@ynov.com>
2023-04-13 16:04:10 +0200

    test kamans sur un dataset

--- scripts/k-mans.py
+++ scripts/k-mans.py
@@ -1,45 +1,43 @@
-def __init__(data, n_clusters, max_iter=100, random_state=123):  # initialisation du cluster 
-       # cette fonction permet de charger un dataset pour entrainer un model de Kmans 
-       # specifier les arguments :
-
-        data.n_clusters = n_clusters 
-        data.max_iter = max_iter
-        data.random_state = random_state
-
-        # n_clusters : nombre de cluster 
-         # max_iter : valeur maximal iterration 
-         # random_state : initialisation d'une valeur possible aleatoire 
- # Initialisons les centroïdes en mélangeant d’abord le jeu de données
-  def initializ_centroids(data, X):
-        np.random.RandomState(data.random_state)
-        random_idx = np.random.permutation(X.shape[0])
-        centroids = X[random_idx[:data.n_clusters]]
-        return centroids
-        
-# itérer jusqu’à ce qu’il n’y ait plus de changement aux centroïdes.
-# Calculez la somme de la distance au carré entre les points de données et tous les centroïdes
- def compute_distance(data, X, centroids):
-        distance = np.zeros((X.shape[0], data.n_clusters))
-        for k in range(data.n_clusters):
-            row_norm = norm(X - centroids[k, :], axis=1)
-            distance[:, k] = np.square(row_norm)
-        return distance
-def find_closest_cluster(data, distance):
-        return np.argmin(distance, axis=1)
-#Affectez chaque point de données au cluster le plus proche (centroïde)
- def compute_sse(data, X, labels, centroids):
-        distance = np.zeros(X.shape[0])
-        for k in range(data.n_clusters):
-            distance[labels == k] = norm(X[labels == k] - centroids[k], axis=1)
-        return np.sum(np.square(distance))
-#alculez les centroïdes pour les clusters en prenant la moyenne de tous les points de données appartenant à chaque cluster.
-def fit(data, X):
-        data.centroids = data.initializ_centroids(X)
-        for i in range(data.max_iter):
-            old_centroids = data.centroids
-            distance = data.compute_distance(X, old_centroids)
-            data.labels = data.find_closest_cluster(distance)
-            data.centroids = data.compute_centroids(X, data.labels)
-            if np.all(old_centroids == data.centroids):
-                break
-        data.error = data.compute_sse(X, data.labels, data.centroids)
+import numpy as np
+from scipy.spatial.distance import cdist 
+from sklearn.datasets import load_digits
+from sklearn.decomposition import PCA
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
+#Loading the required modules
+ 
+import numpy as np
+from scipy.spatial.distance import cdist 
+from sklearn.datasets import load_digits
+from sklearn.decomposition import PCA
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
+ 
+#Defining our function 
+def kmeans(x,k, no_of_iterations):
+    x : data
+    idx = np.random.choice(len(x), k, replace=False)
+    #Randomly choosing Centroids 
+    centroids = x[idx, :] #Step 1
+     
+    #finding the distance between centroids and all the data points
+    distances = cdist(x, centroids ,'euclidean') #Step 2
+     
+    #Centroid with the minimum Distance
+    points = np.array([np.argmin(i) for i in distances]) #Step 3
+     
+    #Repeating the above steps for a defined number of iterations
+    #Step 4
+    for _ in range(no_of_iterations): 
+        centroids = []
+        for idx in range(k):
+            #Updating Centroids by taking mean of Cluster it belongs to
+            temp_cent = x[points==idx].mean(axis=0) 
+            centroids.append(temp_cent)
+ 
+        centroids = np.vstack(centroids) #Updated Centroids 
+         
+        distances = cdist(x, centroids ,'euclidean')
+        points = np.array([np.argmin(i) for i in distances])
+         
+    return points 
