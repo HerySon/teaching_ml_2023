5dbf203b74737e2157e043211c9b36754780c0a5
Pista13 <theo.pistarino@ynov.com>
2023-04-14 02:01:59 +0200

    encode_categorical_variables

--- scripts/cleaning_dataframe.py
+++ scripts/cleaning_dataframe.py
@@ -1,10 +1,12 @@
 import pandas as pd
 from data_loader import *
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 
 class DataCleaner:
-    def __init__(self, df, num_imput='mean'):
+    def __init__(self, df, num_imput='mean', cat_encoding='label'):
         self.df = df
         self.num_imput = num_imput
+        self.cat_encoding = cat_encoding
         
     def count_duplicated_values(self):
         """Compte les valeurs dupliquées pour chaque caractéristique
@@ -37,10 +39,29 @@ class DataCleaner:
         print("\nNombre de valeurs manquantes après l'imputation : %s" % self.df.isnull().sum().sum())
         return self.df
 
+    def encode_categorical_variables(df, encoding_method='onehot'):
+        """Encode categorical variables
+        Args:
+            df (DataFrame) : input dataframe
+            encoding_method (str) : encoding method, either 'onehot' or 'label'
+        Returns:
+            df (DataFrame) : return encoded dataframe
+        """
+        categorical_cols = df.select_dtypes(include=['object']).columns
+        if encoding_method == 'onehot':
+            df = pd.get_dummies(df, columns=categorical_cols, prefix=categorical_cols, prefix_sep='_', drop_first=True)
+        elif encoding_method == 'label':
+            le = LabelEncoder()
+            for col in categorical_cols:
+                df[col] = le.fit_transform(df[col].astype(str))
+        return df
+
     def cleaning(self):
         self.drop_duplicated_values()
 
         self.impute_missing_values()
+
+        self.encode_categorical_variables()
 
 if __name__ == "__main__":
 
