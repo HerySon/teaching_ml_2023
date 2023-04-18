e5904aa4a83653975d5757820c0afc1e656a915e
Nicolas Rochet <nicolas.rochet@protonmail.com>
2023-04-18 21:43:35 +0200

    feat: adds notebook for reading data

--- scripts/data_loader.py
+++ scripts/data_loader.py
@@ -4,7 +4,7 @@
 import yaml
 import pandas as pd
 
-def read_config(file_path='./config.yaml'):
+def read_config(file_path='config.yaml') -> None:
     """Reads configuration file
     Args:
         file_path (str, optional): file path
@@ -14,20 +14,23 @@ def read_config(file_path='./config.yaml'):
     with open(file_path, "r") as f:
         return yaml.safe_load(f)
 
-def get_data(file_path=None, nrows=None):
-    """Loads data
+def get_data(file_path=None, nrows=None, verbose=False) -> pd.DataFrame:
+    """Loads data into a pandas DataFrame
     Args:
         file_path (str, optional): file path of dataset
             By default load data set from static web page
         nrows (int, optional): number or rows to loads from dataset
             By default loads all dataset  
+        verbose (bool): whether to get verbose output    
     Returns:
         dataframe: output dataframe
     """
     if file_path is None:
         cfg = read_config()
         file_path = cfg['paths']['eng_dataset']
-    print("Reading dataset ...")    
+    if verbose:    
+        print("Reading dataset ...")  
+             
     return pd.read_csv(file_path,sep="\t", encoding="utf-8",
                        nrows=nrows, low_memory=False)
 
