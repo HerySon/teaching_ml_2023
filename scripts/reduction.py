0d6900419af190ea2e8f4427018e34051948228d
santoudllo <mamadoualimou.diallo@ynov.com>
2023-04-19 20:13:21 +0200

    correction

--- scripts/reduction.py
+++ scripts/reduction.py
@@ -1,53 +1,35 @@
-Importer numpy en tant que NP
-Importer des pandas en tant que 
-Importer uMap. umap_ comme umap
-de sklearn. BaseEstimator d’importation de base, TransformerMixin
-de sklearn. prétraitement importation StandardScaler
-#de l’algorithme UMAP
- def fit(self, X, y=Aucun, precomputed_distances=Aucun):
-        si soi-même. métrique == « précalculé »:
-            si precomputed_distances est Aucun:
-                raise ValueError(
-                    « Les distances précalculées doivent être fournies si métrique \
- est précalculé.
-                )
-            # prepare X for network formation
-            soi-même. _X = X
-            # Generate the graph over precalculated distances
-            return super(). fit(precomputed_distances, y)
-        sinon:
-            return super(). fit(X, y)
+import pandas as pd
+from sklearn.decomposition import KernelPCA
 
-    def fit_transform(self, X, y=Aucun, precomputed_distances=Aucun):
+def reduce_dim_NLPCA(df, kernel='rbf', gamma=1.0, n_components=2):
+    """
+    df : data
+    """
+    #      Selecting numeric columns
+    numeric_cols = df.select_dtypes(include=[int, float]).columns
+    
+    # Removing columns with missing values
+    df_numeric = df[numeric_cols].dropna()
+    
+    #Création a KernelPCA object for nonlinear data transformation
+    kpca = KernelPCA(n_components=n_components, kernel=kernel, gamma=gamma)
+    
+    #Fit and transform from the KernelPCA object to the df_numeric data
+    X_kpca = kpca.fit_transform(df_numeric)
+    
+     #Création of a pandas dataframe with projected data in n_components dimensions
+    df_proj = pd.DataFrame(X_kpca, columns=[f'dim{i}' for i in range(1, n_components+1)])
+    
+   # Adding product information
+    df_info = df[['code', 'product_name', 'categories', 'brands']]
+    df_proj = pd.concat([df_proj, df_info], axis=1)
+    
+    return df_proj
 
-        si soi-même. métrique == « précalculé »:
-            si precomputed_distances est Aucun:
-                raise ValueError(
-                    « Les distances précalculées doivent être fournies si métrique \
- est précalculé.
-        )
-            
-            soi-même. _X = X
-            
-            return super(). fit_transform(precomputed_distances, y)
-        sinon:
-            return super(). fit_transform(X, y)
- 
+    """
+df = pd.read_csv('openfoodfacts.csv', delimiter='\t', low_memory=False)
 
-de sklearn.  Pipeline d’importation Pipeline
-
-
-## Create a pipeline with UMAPTransformer and a classifier
- pipeline = pipeline([
-    ('umap', UMAPTransformer()),
-    ('classifier', RandomForestClassifier())
-])
-
-# Divide the dataset into train and test set
-de sklearn. model_selection importer train_test_split
-X_train, X_test, y_train, y_test = train_test_split(df. drop(columns=['target']), df['target'], test_size=0.2, random_state=42)
-
-# Train and evaluate the model
-pipeline. fit(X_train, y_train)
-précision = pipeline. score(X_test, y_test)
-print(f"Exactitude : {exactitude} »)
+df_nlpca = reduce_dim_NLPCA(df, kernel='cosine', gamma=0.1, n_components=3)
+display
+print(df_nlpca.head())
+    """
