edf5fff0a4e31d704dfa40d1e3840822e1ba54a8
santoudllo <mamadoualimou.diallo@ynov.com>
2023-04-23 21:08:24 +0200

    correction effectuée

--- scripts/plongement.py
+++ scripts/plongement.py
@@ -1,56 +1,23 @@
-def preprocessdata(data):
-    """
-        Preprocess the data with StandardScalar and Label Encoder
-    :param data: input dataframe of training or test set 
-    """
-    labels = data['LABELS']
-    features = data.drop(['LABELS'], axis=1)
-    columns = features.columns
-    enc = LabelEncoder()
-    enc.fit(labels)
-    labels = enc.transform(labels)
-    features = StandardScaler().fit_transform(features)
-    return features, labels, columns, data['LABELS']
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+def apply_random_forest(train_X, test_X, train_y, test_y, n_estimators=100, max_depth=None):
+    # Initialize the Random Forest model with the given hyperparameters
+    model = RandomForestClassifier(n_estimators=n_estimators, max_depth=max_depth)
 
-Xtrain, Ytrain, COLUMNS, ACTUAL_LABELS = preprocessdata(TRAINDATA)
-Xtest, Ytest, _, _ = preprocessdata(TESTDATA)
-"""
-Before applying feature extraction techniques such as PCA or LDA, we will check the performance of the Random Forest model on the input dataset.
+    # Fit the model to the training data
+    model.fit(train_X, train_y)
 
-"""
-def applyrandomforest(trainX, testX, trainY, testY):
-    """
-        Apply Random forest on input dataset.
-    """
-    start = time.process_time()
-    forest = RandomForestClassifier(n_estimators=700, max_features='sqrt', max_depth=15)
-    forest.fit(trainX, trainY)
-    print("Time Elapsed: %s secs" % (time.process_time() - start))
-    prediction = forest.predict(testX)
-    print("Classification Report after applying Random Forest: ")
-    print("----------------------------------------------------")
-    print(classification_report(testY, prediction))
-    """
-    Let's apply the PCA to our input dataset
-    """
-# Fitting the PCA algorithm with our Data
-pca = PCA()
-pca.fit(trainX)
-# Plotting the Cumulative Summation of the Explained Variance
-plt.figure()
-plt.plot(np.cumsum(pca.explained_variance_ratio_))
-plt.xlabel('Number of Components')
-plt.ylabel('Cumulative Explained Variance')  # for each component
-plt.title('Segmentation Dataset Explained Variance')
-plt.show(block=True)
+    # Use the model to make predictions on the test data
+    y_pred = model.predict(test_X)
 
-"""
- Apply t-SNE for better visualization
-"""
+    # Calculate the accuracy of the model on the test data
+    accuracy = accuracy_score(test_y, y_pred)
+
+    return model, y_pred, accuracy
 lda = LinearDiscriminantAnalysis(n_components=2)
-lda.fit(trainX, trainY)
+lda.fit(train_X, train_y)
 # transform the data onto the first two principal components
-trainX_lda = lda.transform(trainX)
+trainX_lda = lda.transform(train_X)
 colors = ["#476A2A", "#7851B8", "#BD3430", "#4A2D4E", "#875525",
           "#A83683", "#4E655E"]
 plt.figure(figsize=(10, 10))
@@ -69,14 +36,14 @@ We will apply t-SNE which is available at scikit-learn.s
  # Apply tSNE from Manifold learning for better visualization
     tsne = TSNE(random_state=21)
     # use fit_transform instead of fit, as TSNE has no transform method
-    trainX_tsne = tsne.fit_transform(trainX)
+    trainX_tsne = tsne.fit_transform(train_X)
     plt.figure(figsize=(10, 10))
     plt.xlim(trainX_tsne[:, 0].min(), trainX_tsne[:, 0].max())
     plt.ylim(trainX_tsne[:, 1].min(), trainX_tsne[:, 1].max())
     for i in range(len(trainX_tsne)):
         # actually plot the digits as text instead of using scatter
-        plt.text(trainX_tsne[i, 0], trainX_tsne[i, 1], str(trainY[i]),
-                 color=colors[trainY[i]], fontdict={'weight': 'bold', 'size': 9})
+        plt.text(trainX_tsne[i, 0], trainX_tsne[i, 1], str(train_y[i]),
+                 color=colors[train_y[i]], fontdict={'weight': 'bold', 'size': 9})
     plt.xlabel("t-SNE feature 0")
     plt.ylabel("t-SNE feature 1")
     plt.show(block=True)
