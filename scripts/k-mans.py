8acf7365fc0e07d79cb9da7deda4ec56eee6be9e
santoudllo <mamadoualimou.diallo@ynov.com>
2023-04-23 19:45:58 +0200

    modification

--- scripts/k-mans.py
+++ scripts/k-mans.py
@@ -1,43 +1,73 @@
 import numpy as np
-from scipy.spatial.distance import cdist 
-from sklearn.datasets import load_digits
-from sklearn.decomposition import PCA
-from sklearn.cluster import KMeans
-import matplotlib.pyplot as plt
-#Loading the required modules
- 
-import numpy as np
-from scipy.spatial.distance import cdist 
-from sklearn.datasets import load_digits
-from sklearn.decomposition import PCA
-from sklearn.cluster import KMeans
-import matplotlib.pyplot as plt
- 
-#Defining our function 
-def kmeans(x,k, no_of_iterations):
-    x : data
-    idx = np.random.choice(len(x), k, replace=False)
-    #Randomly choosing Centroids 
-    centroids = x[idx, :] #Step 1
-     
-    #finding the distance between centroids and all the data points
-    distances = cdist(x, centroids ,'euclidean') #Step 2
-     
-    #Centroid with the minimum Distance
-    points = np.array([np.argmin(i) for i in distances]) #Step 3
-     
-    #Repeating the above steps for a defined number of iterations
-    #Step 4
-    for _ in range(no_of_iterations): 
-        centroids = []
-        for idx in range(k):
-            #Updating Centroids by taking mean of Cluster it belongs to
-            temp_cent = x[points==idx].mean(axis=0) 
-            centroids.append(temp_cent)
- 
-        centroids = np.vstack(centroids) #Updated Centroids 
-         
-        distances = cdist(x, centroids ,'euclidean')
-        points = np.array([np.argmin(i) for i in distances])
-         
-    return points 
+import math
+import matplotlib.pylab as plt
+from scipy.spatial.distance import pdist, squareform
+
+
+class KMeanPrecomputed:
+    def __init__(data, distance_matrix):
+        data.distances = distance_matrix
+        data.point_count = distance_matrix.shape[0]
+
+        data.nearest_centroids = None
+        data.centroids = None
+        data.cluster_count = 0
+        data.cost = 0
+
+    def run(data, cluster_count=2, max_iterations=1000):
+        data.cluster_count = cluster_count
+
+        data.clusters_init()
+        last_cost = 0
+        data.clusters_match()
+        for i in range(max_iterations):
+            data.clusters_move()
+            data.clusters_match()
+
+            data.cost = data.cluster_cost()
+
+            if last_cost == data.cost:
+                break
+            last_cost = data.cost
+
+        return data.nearest_centroids
+
+    def clusters_init(data):
+        data.centroids = np.random.choice(data.point_count, data.cluster_count, replace=False)
+        data.nearest_centroids = np.random.choice(data.cluster_count, data.point_count)
+
+    def clusters_match(data):
+        data.nearest_centroids = data.distances[:, data.centroids].argmin(axis=1)
+
+    def clusters_move(data):
+        for i, centroid in enumerate(data.centroids):
+            points_in_cluster = data.nearest_centroids == i
+            new_centroid_index = data.distances[points_in_cluster][:, points_in_cluster].sum(axis=0).argmin()
+            new_centroid = np.argwhere(points_in_cluster.cumsum() == new_centroid_index + 1)[0][0]
+            data.centroids[i] = new_centroid
+
+    def cluster_cost(data):
+        return data.distances[range(data.point_count), data.centroids[data.nearest_centroids]].sum() / data.point_count
+
+
+def demo():
+    dat = 1000
+    a = np.empty((4*dat,2),float)
+    for i in range(dat):
+        a[i] = np.random.multivariate_normal([0,0],[[1,0.1],[1,.7]])
+        a[i+dat] = np.random.multivariate_normal([3,2],[[0.5,0],[0,2]])
+        a[i+2*dat] = np.random.multivariate_normal([3,2],[[0.5,0],[0,2]])
+        a[i+3*dat] = np.random.multivariate_normal([0,4],[[0.1,0],[0,.1]])
+
+    distance_matrix = squareform(pdist(a, 'euclidean'))
+    km = KMeanPrecomputed(distance_matrix)
+    labels = km.run(3)
+    z = (labels == 2)
+    x = (labels == 1)
+    y = (labels == 0)
+
+
+    plt.plot(a[x,0],a[x,1],"ro")
+    plt.plot(a[y,0],a[y,1],"bo")
+    plt.plot(a[z, 0],a[z, 1],"go")
+    plt.show()
