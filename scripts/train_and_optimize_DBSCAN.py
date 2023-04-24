6502b0a249170ab4b4411f0cc90cfa561e8d7768
keagnon <keagnon.gbe@gmail.com>
2023-04-24 12:45:21 +0200

    Tache5.1 grace : modification effectuée

--- scripts/train_and_optimize_DBSCAN.py
+++ scripts/train_and_optimize_DBSCAN.py
@@ -1,4 +1,13 @@
-def train_and_optimize_DBSCAN(data, feature_cols, eps_min=0.1, eps_max=1.0, eps_step=0.1, min_samples_min=2, min_samples_max=10, min_samples_step=1):
+import pandas as pd
+import numpy as np
+from sklearn.cluster import DBSCAN
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import silhouette_score  
+from sklearn.model_selection import ParameterGrid
+
+def train_and_optimize_DBSCAN(data, feature_cols, scale_data=False,
+                              param_grid={'eps': [0.1, 0.5, 1.0], 
+                                          'min_samples': [2, 5, 10]}):
     """
     --------------------------------------------------------------------------
     Trains and optimizes DBSCAN on a given dataset.
@@ -10,18 +19,10 @@ def train_and_optimize_DBSCAN(data, feature_cols, eps_min=0.1, eps_max=1.0, eps_
             The dataset to be used for training and optimization.
         - feature_cols (list): 
             A list of column names for the features to be used for training.
-        - eps_min (float): 
-            The minimum value of the eps parameter to try during optimization. Default is 0.1.
-        - eps_max (float): 
-            The maximum value of the eps parameter to try during optimization. Default is 1.0.
-        - eps_step (float): 
-            The step size for the eps parameter during optimization. Default is 0.1.
-        - min_samples_min (int): 
-            The minimum value of the min_samples parameter to try during optimization. Default is 2.
-        - min_samples_max (int): 
-            The maximum value of the min_samples parameter to try during optimization. Default is 10.
-        - min_samples_step (int):
-            The step size for the min_samples parameter during optimization. Default is 1.
+        - scale_data (bool):
+            Whether to scale the data or not. Default is False.
+        - param_grid (dict):
+            The grid of parameters to search over. Default is {'eps': [0.1, 0.5, 1.0], 'min_samples': [2, 5, 10]}.
     -----------------------------------------------------------------------------------
     Returns:
     -----------------------------------------------------------------------------------
@@ -29,31 +30,24 @@ def train_and_optimize_DBSCAN(data, feature_cols, eps_min=0.1, eps_max=1.0, eps_
         as determined by the silhouette score.
     -------------------------------------------------------------------------------
     """
-    
-    import pandas as pd
-    import numpy as np
-    from sklearn.cluster import DBSCAN
-    from sklearn.preprocessing import StandardScaler
-    from sklearn.metrics import silhouette_score
 
-    
     # Select the feature columns
     X = data[feature_cols].values
     
-    # Normalize the data
-    scaler = StandardScaler()
-    X = scaler.fit_transform(X)
+    # Normalize the data if requested
+    if scale_data:
+        scaler = StandardScaler()
+        X = scaler.fit_transform(X)
     
     # Try different values of eps and min_samples and evaluate them
     scores = []
-    for eps in np.arange(eps_min, eps_max + eps_step, eps_step):
-        for min_samples in range(min_samples_min, min_samples_max + min_samples_step, min_samples_step):
-            dbscan = DBSCAN(eps=eps, min_samples=min_samples)
-            dbscan.fit(X)
-            if len(set(dbscan.labels_)) > 1:  # Skip if only one cluster is found
-                score = silhouette_score(X, dbscan.labels_)
-                scores.append((eps, min_samples, score))
-                print("DBSCAN with eps={}, min_samples={} - Silhouette Score: {}".format(eps, min_samples, score))
+    for params in ParameterGrid(param_grid):
+        dbscan = DBSCAN(**params)
+        dbscan.fit(X)
+        if len(set(dbscan.labels_)) > 1:  # Skip if only one cluster is found
+            score = silhouette_score(X, dbscan.labels_)
+            scores.append((params['eps'], params['min_samples'], score))
+            print("DBSCAN with eps={}, min_samples={} - Silhouette Score: {}".format(params['eps'], params['min_samples'], score))
     
     # Find the optimal values for eps and min_samples
     if len(scores) > 0:
@@ -66,13 +60,4 @@ def train_and_optimize_DBSCAN(data, feature_cols, eps_min=0.1, eps_max=1.0, eps_
 
   
 if __name__ == "__main__":  
-    
-   #Example of application
-    np.random.seed(123)
-    data = pd.DataFrame({
-        'feature1': np.random.randn(100),
-        'feature2': np.random.randn(100),
-        'feature3': np.random.randn(100)
-    })
-
-    optimal_params = train_and_optimize_DBSCAN(data, ['feature1', 'feature2', 'feature3'])
+    train_and_optimize_DBSCAN(data, feature_cols, scale_data=True, param_grid={'eps': [0.1, 0.5, 1.0], 'min_samples': [2, 5, 10], 'metric': ['euclidean', 'manhattan']})
