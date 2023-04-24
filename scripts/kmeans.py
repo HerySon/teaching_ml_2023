65073555b1b9f64e87971c5a306608fbf479a9f3
keagnon <keagnon.gbe@gmail.com>
2023-04-24 12:20:35 +0200

    Tache 5.0 grace : Modification effectuée (toutes les modifications demandés)

--- scripts/kmeans.py
+++ scripts/kmeans.py
@@ -1,5 +1,11 @@
-
-def train_and_optimize_kmeans(data, feature_cols, num_clusters=3, max_clusters=10):
+import pandas as pd
+import numpy as np
+from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import silhouette_score
+    
+def train_and_optimize_kmeans(data, feature_cols, num_clusters=3, max_clusters=10, use_pca=True, use_scaler=True, **kwargs):
     """
     ----------------------------------------------------------
     Goal : 
@@ -8,39 +14,40 @@ def train_and_optimize_kmeans(data, feature_cols, num_clusters=3, max_clusters=1
     ---------------------------------------------------------
     Args:
     -----------------------------------------------------------
-        data(str): OpenFoodFacts dataset file.
+        data: OpenFoodFacts dataframe.
         feature_cols (list): A list of column names for the features to be used for training.
         num_clusters (int): The number of clusters to use for the initial K-means training.
                             Default is 3.
         max_clusters (int): The maximum number of clusters to try during the optimization.
                             Default is 10.
+        use_pca (bool): Whether to use PCA for dimensionality reduction. Default is True.
+        use_scaler (bool): Whether to use StandardScaler for data normalization. Default is True.
+        **kwargs : additional parameters for KMeans model
+        
     -------------------------------------------------------------
     Returns:
     --------------------------------------------------------------
         The optimal number of clusters, as determined by the silhouette score.
     ------------------------------------------------------------------------
     """
-    import pandas as pd
-    import numpy as np
-    from sklearn.cluster import KMeans
-    from sklearn.decomposition import PCA
-    from sklearn.preprocessing import StandardScaler
-    from sklearn.metrics import silhouette_score
+
 
     
     # Select the feature columns
     X = data[feature_cols].values
     
     # Apply dimensionality reduction if needed
-    pca = PCA(n_components=2)
-    X = pca.fit_transform(X)
+    if use_pca:
+        pca = PCA(n_components=2)
+        X = pca.fit_transform(X)
     
     # Normalize the data if needed
-    scaler = StandardScaler()
-    X = scaler.fit_transform(X)
+    if use_scaler:
+        scaler = StandardScaler()
+        X = scaler.fit_transform(X)
     
     # Train the initial K-means model
-    kmeans = KMeans(n_clusters=num_clusters, random_state=42)
+    kmeans = KMeans(n_clusters=num_clusters, random_state=42, **kwargs)
     kmeans.fit(X)
     
     # Evaluate the initial model
@@ -51,7 +58,7 @@ def train_and_optimize_kmeans(data, feature_cols, num_clusters=3, max_clusters=1
     # Try different numbers of clusters and evaluate them
     scores = []
     for i in range(2, max_clusters+1):
-        kmeans = KMeans(n_clusters=i, random_state=42)
+        kmeans = KMeans(n_clusters=i, random_state=42, **kwargs)
         kmeans.fit(X)
         score = silhouette_score(X, kmeans.labels_)
         scores.append(score)
@@ -70,5 +77,5 @@ if __name__ == "__main__":
     feature_cols = ['col1', 'col2', 'col3']
     num_clusters = 3
     max_clusters = 10
-
-    optimal_clusters = train_and_optimize_kmeans(data, feature_cols, num_clusters, max_clusters)
+    kmeans_kwargs = {'init': 'random', 'n_init': 10, 'max_iter': 300, 'tol': 1e-04}
+    optimal_clusters = train_and_optimize_kmeans(data, feature_cols, num_clusters, max_clusters, True, True, **kmeans_kwargs)
