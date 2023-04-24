af3b169c3130eb0f35b917e655aff5f76de90756
keagnon <keagnon.gbe@gmail.com>
2023-04-24 17:57:40 +0200

    Modification effectuée

--- scripts/reduce_dimensions.py
+++ scripts/reduce_dimensions.py
@@ -1,71 +1,55 @@
+import pandas as pd
+from sklearn.decomposition import PCA
+from sklearn.manifold import TSNE
 
-def reduce_dimensions(data, method="pca", n_components=None, perplexity=None):
+def reduce_dimensions(data, method, features=None, perplexity=None, random_state=None):
     """
-    ------------------------------------------------------------------------------
-    Reduces the number of dimensions in the input data using the specified method.
-    -----------------------------------------------------------------------------
-    Parameters:
-    -----------------------------------------------------------------------------
-        data (pandas.DataFrame or numpy.ndarray): The input data to be transformed.
-        method (str): The method to use for dimensionality reduction. Default is "pca".
-        n_components (int or None): The number of components to keep. Only applicable 
-                                                                     for "pca" and "ica".
-                                    If None, all components are kept. Default is None.
-        perplexity (float or None): The perplexity parameter for t-SNE. Only applicable
-                                                                        for "tsne".
-                                    If None, the default value is used. Default is None.
-    ---------------------------------------------------------------------------
-    Returns:
-    --------------------------------------------------------------------------
-        The transformed data.
-    --------------------------------------------------------------------------
+        -----------------------------------------------------------------------
+        Reduces the number of dimensions in the input 
+        data using the specified method.
+        -----------------------------------------------------------------------
+        Parameters:
+        -----------------------------------------------------------------------
+        - data (pandas.DataFrame): The input data to reduce dimensions on.
+        - method (str): The dimensionality reduction method to use. 
+        Either "pca" or "tsne".
+        - features (list of str, optional): The feature columns to use for 
+        dimensionality reduction. Required if method is "pca".
+        - perplexity (float, optional): The perplexity hyperparameter for t-SNE.
+        Required if method is "tsne".
+        - random_state (int, optional): The random state for reproducibility.
+        Required if method is "tsne".
+        ---------------------------------------------------------------------
+        Returns:
+        ---------------------------------------------------------------------
+        The reduced dataset.
+        ---------------------------------------------------------------------
     """
-    
-    import pandas as pd
-    import numpy as np
-    from sklearn.decomposition import PCA
-    from sklearn.manifold import TSNE
-    
-    if isinstance(data, pd.DataFrame):
-        X = data.values
+    if method == 'pca':
+        # Perform PCA to reduce dimensions
+        pca = PCA(n_components=2) # reduce to 2 dimensions for visualization
+        reduced_data = pd.DataFrame(pca.fit_transform(data[features]), columns=['PC1', 'PC2'])
+    elif method == 'tsne':
+        # Perform t-SNE to reduce dimensions
+        tsne = TSNE(n_components=2, perplexity=perplexity, random_state=random_state)
+        reduced_data = pd.DataFrame(tsne.fit_transform(data[features]), columns=['t-SNE 1', 't-SNE 2'])
     else:
-        X = data
+        raise ValueError('Unsupported dimensionality reduction method: {}'.format(method))
     
-    if method == "pca":
-        pca = PCA(n_components=n_components)
-        X_transformed = pca.fit_transform(X)
-    elif method == "tsne":
-        tsne = TSNE(n_components=2, perplexity=perplexity, random_state=42)
-        X_transformed = tsne.fit_transform(X)
-    else:
-        raise ValueError("Invalid method specified. Valid options are 'pca' and 'tsne'.")
+    return reduced_data
     
-    return X_transformed
 
 
+if __name__ == '__main__':
 
+    # Select some columns to use as features
+    features = ['energy_100g', 'fat_100g', 'carbohydrates_100g', 'sugars_100g', 'fiber_100g', 'proteins_100g']
 
-if __name__ == "__main__":  
-    
-   #Example of application
-   from sklearn.datasets import load_digits
-   # Load the digits dataset
-   digits = load_digits()
+    # Reduce dimensions using PCA
+    reduced_data_pca = reduce_dimensions(data, 'pca', features=features)
 
-    # Apply dimensionality reduction using PCA
-   X_pca = reduce_dimensions(digits.data, method="pca", n_components=2)
-
-   # Apply dimensionality reduction using t-SNE
-   X_tsne = reduce_dimensions(digits.data, method="tsne", perplexity=30)
-
-   # Display the reduced data
-   import matplotlib.pyplot as plt
-
-   fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(10, 5))
-
-   ax[0].scatter(X_pca[:, 0], X_pca[:, 1], c=digits.target)
-   ax[0].set_title("PCA")
-   ax[1].scatter(X_tsne[:, 0], X_tsne[:, 1], c=digits.target)
-   ax[1].set_title("t-SNE")
-
-   plt.show()
+    # Reduce dimensions using t-SNE
+    reduced_data_tsne = reduce_dimensions(data, 'tsne', features=features, perplexity=30, random_state=42)
+    # print the shape of the reduced datasets
+    print('PCA reduced dataset shape:', reduced_data_pca.shape)
+    print('t-SNE reduced dataset shape:', reduced_data_tsne.shape)
